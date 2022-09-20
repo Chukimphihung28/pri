fbe424dcf1b922e1938e570e2380bb2dc1751452
Chukimphihung28 <88919147+Chukimphihung28@users.noreply.github.com>
2022-09-20 14:34:16 +0700

    h

---
+++ getloaisp.php
@@ -0,0 +1,26 @@
+<?php
+include "connect.php";
+$query = "SELECT * FROM `sanpham`";
+$data = mysqli_query($conn,$query);
+$result = array();
+while ($row = mysqli_fetch_assoc($data)) {
+    $result[] = ($row);
+    //
+}
+if (!empty($result)) {
+    # code...
+    $arr = [
+        'success' => true,
+        'message' => "thanh cong",
+        'result' => $result
+    ];
+} else {
+    $arr = [
+        'success' => false,
+        'message' => "k thanh cong",
+        'result' => $result
+    ];
+}
+
+print_r(json_encode($arr))
+?>
